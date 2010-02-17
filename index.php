7a330e6b1ee6767aa08c77449bee16ee14c4d8f7
turbo <turbo>
2010-02-17 20:14:48 +0000

    * Take axle distance > 22 meters on BK3 into account * Put inputs (truck/trailer) between each other instead of trailer below truck * Check for the common limitations for motor veicles

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 $DEBUG = 0;
 
 // Calculate max load and gross load on truck and/or trailer
-// $Id: index.php,v 1.5 2010-02-16 22:37:42 turbo Exp $
+// $Id: index.php,v 1.6 2010-02-17 20:14:47 turbo Exp $
 
-$VERSION = "$Revision: 1.5 $";
+$VERSION = "$Revision: 1.6 $";
 
 // {{{ Defines
 $BK["1"]["FRONT"] = 10000;
@@ -34,6 +34,7 @@ function parse_table($data, $distance) {
 
   // Tables is in meters, but form is in milimeter...
   $d = $distance / 1000;
+  //echo "Distance: '$d'm<br>";
 
   for($i = 0; $data[$i]; $i++) {
 	// Split up the line in three data parts
@@ -53,8 +54,13 @@ function parse_table($data, $distance) {
 	  if($chars[7] == '+') {
 		// Special case for BK3! axle distance > 22 meter
 		// Ex:	22.00<+0.20=37.50+00.25
-		// TODO
-		die("Sorry, not yet implemented!<br>");
+		$res = preg_split("/\+/", $vals[2]);
+
+		for($j = $vals[0], $k = $res[0]; $d > $j; $j += $vals[1], $k += $res[1])
+		  ;
+
+		$j -= $vals[1]; $k -= $res[1];
+		return($k);
 	  } else {
 		if( (($d >= $vals[0]) and ($d < $vals[1])) or
 			(($d >= $vals[0]) and ($vals[1] == '0')) )
@@ -78,95 +84,118 @@ if(!$_REQUEST["action"]) {
     <table>
       <form method="post" name="berakning":
         <tr>
-          <td colspan="3"><h1>Bil:</h1></td>
+          <td colspan="2"><h1>Bil:</h1></td>
+          <td align="right"><input type="checkbox" name="truck_save">Spara</td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td colspan="2"><h1>Sl&auml;p:</h1></td>
+          <td align="right"><input type="checkbox" name="trailer_save">Spara</td>
         </tr>
 
         <tr>
 	      <td>Tj&auml;nstevikt</td>
-          <td><input type="text" name="truck_weight"></td>
+          <td><input type="text" name="truck_weight" tabindex="1"></td>
           <td>Kg</td>
-          <td><input type="checkbox" name="truck_weight_save"<?php if($_REQUEST["truck_weight_save"]) { echo " CHECKED"; } ?></td>
-        </tr>
-
-        <tr>
-          <td>Max belastning<br>Framaxel</td>
-          <td><input type="text" name="truck_load_front"></td>
-          <td>Kg</td>
-          <td><input type="checkbox" name="truck_load_front_save"></td>
-        </tr>
-
-        <tr>
-          <td>Max belastning<br>Bakaxel</td>
-          <td><input type="text" name="truck_load_back"></td>
-          <td>Kg</td>
-          <td><input type="checkbox" name="truck_load_back_save"></td>
-        </tr>
-
-        <tr>
-          <td>Axelavst&aring;nd</td>
-          <td><input type="text" name="truck_axle"></td>
-          <td>mm</td>
-          <td><input type="checkbox" name="truck_axle_save"></td>
-        </tr>
-
-        <tr>
-          <td>Kopplingsavst&aring;nd</td>
-          <td><input type="text" name="truck_link"></td>
-          <td>mm</td>
-          <td><input type="checkbox" name="truck_link_save"></td>
-        </tr>
-
-        <tr>
-          <td></td>
-          <td colspan="2"><font size="-2">* Endast n&ouml;dv&auml;ndig om fordonst&aring;g ber&auml;knas</font></td>
-        </tr>
-
-        <!-- ---------------------------------- -->
-
-        <tr>
-          <td colspan="3"><h1>Sl&auml;p:</h1></td>
-        </tr>
-
-        <tr>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
           <td>Tj&auml;nstevikt</td>
-          <td><input type="text" name="trailer_weight"></td>
+          <td><input type="text" name="trailer_weight" tabindex="11"></td>
           <td>Kg</td>
-          <td><input type="checkbox" name="trailer_weight_save"<?php if($_REQUEST["trailer_weight_save"]) { echo " CHECKED"; } ?></td>
         </tr>
 
         <tr>
           <td>Max belastning<br>Framaxel</td>
-          <td><input type="text" name="trailer_load_front"></td>
+          <td><input type="text" name="truck_load_front" tabindex="2"></td>
           <td>Kg</td>
-          <td><input type="checkbox" name="trailer_load_front_save"></td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>Max belastning<br>Framaxel</td>
+          <td><input type="text" name="trailer_load_front" tabindex="12"></td>
+          <td>Kg</td>
         </tr>
 
         <tr>
           <td>Max belastning<br>Bakaxel</td>
-          <td><input type="text" name="trailer_load_back"></td>
+          <td><input type="text" name="truck_load_back" tabindex="3"></td>
           <td>Kg</td>
-          <td><input type="checkbox" name="trailer_load_back_save"></td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>Max belastning<br>Bakaxel</td>
+          <td><input type="text" name="trailer_load_back" tabindex="13"></td>
+          <td>Kg</td>
         </tr>
 
         <tr>
           <td>Axelavst&aring;nd</td>
-          <td><input type="text" name="trailer_axle"></td>
+          <td><input type="text" name="truck_axle" tabindex="4"></td>
           <td>mm</td>
-          <td><input type="checkbox" name="trailer_axle_save"></td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>Axelavst&aring;nd</td>
+          <td><input type="text" name="trailer_axle" tabindex="14"></td>
+          <td>mm</td>
         </tr>
 
         <tr>
           <td>Kopplingsavst&aring;nd</td>
-          <td><input type="text" name="trailer_link"></td>
+          <td><input type="text" name="truck_link" tabindex="5"></td>
           <td>mm</td>
-          <td><input type="checkbox" name="trailer_link_save"></td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>Kopplingsavst&aring;nd</td>
+          <td><input type="text" name="trailer_link" tabindex="15"></td>
+          <td>mm</td>
         </tr>
 
         <tr>
           <td></td>
           <td colspan="2"><font size="-2">* Endast n&ouml;dv&auml;ndig om fordonst&aring;g ber&auml;knas</font></td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td></td>
+          <td colspan="2"><font size="-2">* Endast n&ouml;dv&auml;ndig om fordonst&aring;g ber&auml;knas</font></td>
         </tr>
- 
+
+        <tr>
+          <td>Axlar</td>
+          <td>
+            <select name="truck_axles">
+              <option name="single" value="single">Enkelaxel</option>
+              <option name="boggie" value="boggie">Boggie</option>
+              <option name="tripple" value="tripple">Trippleaxel</option>
+            </select>
+          </td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>Axlar</td>
+          <td>
+            <select name="trailer_axles">
+              <option name="single" value="single">Enkelaxel</option>
+              <option name="boggie" value="boggie">Boggie</option>
+              <option name="tripple" value="tripple">Trippleaxel</option>
+            </select>
+          </td>
+        </tr>
+
+        <tr>
+          <td>Boggie/Trippleaxel<br>avst&aring;nd</td>
+          <td><input type="text" name="truck_boggie_dist" tabindex="6"></td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>Boggie/Trippleaxel<br>avst&aring;nd</td>
+          <td><input type="text" name="trailer_boggie_dist" tabindex="16"></td>
+        </tr>
+
+        <tr>
+          <td>V&auml;gv&auml;nlig fj&auml;dring</td>
+          <td><input type="checkbox" name="truck_road_nice" tabindex="7"></td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>V&auml;gv&auml;nlig fj&auml;dring</td>
+          <td><input type="checkbox" name="trailer_road_nice" tabindex="17"></td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td colspan="2"><font size="-2">* Tvillingmontage och luftfj&auml;dring</font></td>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td></td>
+          <td colspan="2"><font size="-2">* Tvillingmontage och luftfj&auml;dring</font></td>
+        </tr>
+
         <!-- ---------------------------------- -->
 
        <tr>
@@ -193,13 +222,14 @@ if(!$_REQUEST["action"]) {
   // }}}
 
   // {{{ 1. Load BKx table file(s)
-  for($bk = 1; $bk <= 3; $bk++) {
-	$name = "table-bk".$bk.".txt";
+  foreach(array("bk1", "bk2", "bk3", "boggie", "tripple") as $bk) {
+	$nr = preg_replace("/^bk([1-3])/", "$1", $bk);
+	$name = "table-$bk".".txt";
 	$fh = fopen($name, 'r') or die("Kan inte öppna bruttoviktstabell filerna!<br>");
 
 	$j = 0;
 	while($line = fgets($fh)) {
-	  $TABLE_DATA[$bk][$j] = trim($line);
+	  $TABLE_DATA[$nr][$j] = trim($line);
 
 	  $j++;
 	}
@@ -217,14 +247,18 @@ if(!$_REQUEST["action"]) {
   for($bk = 1; $bk <= 3; $bk++) {
 	// ab. truck and trailer
 	foreach(array("truck", "trailer") as $type) {
-	  $str = $type."_axle";
-	  if($_REQUEST[$str])
-		$GROSS_BK[$bk][$type] = parse_table($TABLE_DATA[$bk], $_REQUEST[$str]);
+	  $str = $type."_axles";
+	  
+	  if($_REQUEST[$str] == "single") {
+		// => SINGLE
+		$str1 = $type."_axle";
+		if($_REQUEST[$str1])
+		  $GROSS_BK[$bk][$type] = parse_table($TABLE_DATA[$bk], $_REQUEST[$str1]);
+	  } elseif($_REQUEST[$str] == "boggie") {
+		// => BOGGIE
+		// TODO
+	  }
 	}
-
-	// c. train
-	if($_REQUEST["truck_link"] and $_REQUEST["trailer_link"]) 
-	  $GROSS_BK[$bk]["train"] = parse_table($TABLE_DATA[$bk], ($_REQUEST["truck_link"] + $_REQUEST["trailer_link"]));
   }	
 
   if($DEBUG) {
@@ -264,7 +298,37 @@ if(!$_REQUEST["action"]) {
   for($bk = 1; $bk <= 3; $bk++) {
 	foreach(array("truck", "trailer") as $type) {
 	  ($GROSS_BK[$bk][$type] <= $MAX_AXLE[$bk][$type]) ? $LOAD[$bk][$type] = $GROSS_BK[$bk][$type] : $LOAD[$bk][$type] = $MAX_AXLE[$bk][$type];
+
+	  // {{{ Check for common limitations...
+	  if($LOAD[$bk][$type]) {
+		$str1 = $type."_axles";
+		$str2 = $type."_road_nice";
+		
+		if(($_REQUEST[$str1] == 'single') and ($LOAD[$bk][$type] > 18))
+		  // Two axles
+		  $LOAD[$bk][$type] = 18;
+		
+		elseif(($_REQUEST[$str1] == 'boggie') and !$_REQUEST[$str2] and ($LOAD[$bk][$type] > 25))
+		  // Three axles, Not road nice
+		  $LOAD[$bk][$type] = 25;
+		
+		elseif(($_REQUEST[$str1] == 'boggie') and  $_REQUEST[$str2] and ($LOAD[$bk][$type] > 26))
+		  // Three axles, road nice
+		  $LOAD[$bk][$type] = 26;
+		
+		elseif(($_REQUEST[$str1] == 'tripple') and !$_REQUEST[$str2] and ($LOAD[$bk][$type] > 31))
+		  // >= Four axles, Not road nice
+		  $LOAD[$bk][$type] = 31;
+		
+		elseif(($_REQUEST[$str1] == 'tripple') and  $_REQUEST[$str2] and ($LOAD[$bk][$type] > 32))
+		  // >= Four axles, road nice
+		  $LOAD[$bk][$type] = 32;
+	  } else
+		$LOAD[$bk][$type] = 0;
 	}
+
+	$LOAD[$bk][$type] = sprintf("%01.02f", $LOAD[$bk][$type]);
+	// }}}
 
 	// c. train
 	$LOAD[$bk]["train"] = $LOAD[$bk]["truck"] + $LOAD[$bk]["trailer"];
@@ -299,12 +363,21 @@ if(!$_REQUEST["action"]) {
         <td>BK<?php echo "$bk"; ?></td>
         <td><?php echo $LOAD[$bk]["train"]?> ton</td>
         <td>(<?php
-			 $max_load_truck = ((($LOAD[$bk]["truck"] * 1000) - $_REQUEST["truck_weight"]) / 1000);
-			 $max_load_train = ((($LOAD[$bk]["train"] * 1000 ) - ($_REQUEST["truck_weight"] + $_REQUEST["trailer_weight"])) / 1000);
+			 $max_load_truck = (($LOAD[$bk]["truck"] * 1000) -  $_REQUEST["truck_weight"]) / 1000;
+			 $max_load_train = (($LOAD[$bk]["train"] * 1000) - ($_REQUEST["truck_weight"] + $_REQUEST["trailer_weight"])) / 1000;
 			 $max_load_trail = $max_load_train - $max_load_truck;
 
-			 echo $max_load_truck." + ".$max_load_trail." = <b>".$max_load_train."</b>"; ?> ton)</td>
-			 
+			 // Output...
+			 $str  = "";
+			 if($_REQUEST["truck_weight"] and $_REQUEST["trailer_weight"]) {
+			   $str .= $max_load_truck . " + ";
+			   $str .= $max_load_trail . " = ";
+			 }
+
+			 $str .= "<b>".$max_load_train."</b>";
+			 echo "$str";
+?> ton)
+        </td>
       </tr>
 <?php
 	}
@@ -316,6 +389,7 @@ if(!$_REQUEST["action"]) {
   // }}}
 }
 
+// {{{ Copyleft etc
 echo "<p><p><font size=\"-1\">Lastber&auml;rkningsprogram, Copyleft<br>Turbo Fredriksson &lt;turbo@bayour.com&gt;<br>";
 echo "Version".preg_replace("/.*sion: ([0-9].*[0-9]) \$/", "\1", $VERSION);
 echo "</font>";
@@ -327,4 +401,5 @@ echo "</font>";
  * tab-width: 4
  * End:
  */
+// }}}
 ?>
