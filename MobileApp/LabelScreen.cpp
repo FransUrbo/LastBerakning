ace4fcf69e6ea8f97ce058a3a9deead94a0468f7
turbo <turbo>
2010-02-25 16:41:34 +0000

    * Added CVS Id tag. * Translate to Swedish. * Write and format all helpt text labels.

--- MobileApp/LabelScreen.cpp
+++ MobileApp/LabelScreen.cpp
@@ -15,6 +15,12 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 02111-1307, USA.
 */
 
+/* Modified by Turbo Fredriksson <turbo@bayour.com>
+ *
+ * This screen is the help text screen.
+ * $Id: LabelScreen.cpp,v 1.2 2010-02-25 16:41:34 turbo Exp $
+ */
+
 #include <conprint.h> /* lprintfln() */
 
 #include "MAUIex.h"
@@ -22,24 +28,13 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #include "Util.h"
 
 LabelScreen::LabelScreen(Screen *previous) : previous(previous) {
-	mainLayout = createMainLayout("", "back");
+	mainLayout = createMainLayout("", "Tillbaka");
 	listBox = (ListBox*) mainLayout->getChildren()[0];
 	Label *label;
 
-	label = createLabel("BottomRight", 64);
-	label->setHorizontalAlignment(Label::HA_RIGHT);
-	label->setVerticalAlignment(Label::VA_BOTTOM);
-	label->setSkin(gSkin);
-	listBox->add(label);
-
 	label = createLabel
 	(
-		"Lorem ipsum dolor sit amet, consectetur adipisicing elit, "
-		"sed do eiusmod tempor incididunt ut labore et dolore magna "
-		"aliqua. Ut enim ad minim veniam, quis nostrud exercitation "
-		"ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-		"Duis aute irure dolor in reprehenderit in voluptate velit "
-		"esse cillum dolore eu fugiat nulla pariatur.", 
+		"Vikter skrivs direkt i ton, inte kilogram!",
 		32
 	);
 	label->setAutoSizeY();
@@ -47,9 +42,139 @@ LabelScreen::LabelScreen(Screen *previous) : previous(previous) {
 	label->setMultiLine(true);
 	listBox->add(label);
 
-	label = createLabel("automatic resizing", 32);
-	label->setAutoSizeX();
+	label = createLabel
+	(
+		"Programmet stöder:\n"
+		"Enkelaxlade fordon (2 axlar - en fram "
+		"och en bak),\n"
+		"Boggieaxlade fordon (3 axlar - en fram "
+		"och en bak) samt\n"
+		"Trippelaxlade fordon (4 axlar - en fram "
+		"och tre bak).",
+		32
+	);
+	label->setAutoSizeY();
 	label->setSkin(gSkin);
+	label->setMultiLine(true);
+	listBox->add(label);
+
+	label = createLabel
+	(
+		"Programmet stödjer också en kombination av detta (enkel axlat, "
+		"boggie eller trippelaxel fram och ditto bak) etc i alla "
+		"möjliga variationer. Som max sex axlar (trippelaxel fram "
+		"och trippelaxel bak).",
+		32
+	);
+	label->setAutoSizeY();
+	label->setSkin(gSkin);
+	label->setMultiLine(true);
+	listBox->add(label);
+
+	label = createLabel
+	(
+		"Axelavstånd skrivs i millimeter (från registreringsbeviset).",
+		32
+	);
+	label->setAutoSizeY();
+	label->setSkin(gSkin);
+	label->setMultiLine(true);
+	listBox->add(label);
+
+	label = createLabel
+	(
+		"Enkelaxel\n"
+		"Avståndet mellan framaxel och bakaxel.\n"
+		"Exempel: 5900",
+		32
+	);
+	label->setAutoSizeY();
+	label->setSkin(gSkin);
+	label->setMultiLine(true);
+	listBox->add(label);
+
+	label = createLabel
+	(
+		"Boggieaxel\n"
+		"Avståndet mellan framaxel och första bakaxeln + "
+		"avståndet mellan första och sista bakaxeln.\n"
+		"Exempel: 4900+1350",
+		32
+	);
+	label->setAutoSizeY();
+	label->setSkin(gSkin);
+	label->setMultiLine(true);
+	listBox->add(label);
+
+	label = createLabel
+	(
+		"Trippelaxel\n"
+		"Avståndet mellan framaxel och första bakaxeln "
+		"+ avståndet mellan första och andra bakaxeln + "
+		"avståndet mellan andra och sista (tredje) bakaxeln.\n"
+		"Exempel: 3750+1360+1310",
+		32
+	);
+	label->setAutoSizeY();
+	label->setSkin(gSkin);
+	label->setMultiLine(true);
+	listBox->add(label);
+
+	label = createLabel
+	(
+		"Trippel fram och Trippel bak\n"
+		"Exempel: 1530+1530+4185+1360+1310",
+		32
+	);
+	label->setAutoSizeY();
+	label->setSkin(gSkin);
+	label->setMultiLine(true);
+	listBox->add(label);
+
+	label = createLabel
+	(
+		"Programmet räknar automatiskt ut dom avstånd det "
+		"behöver från dessa värden...",
+		32
+	);
+	label->setAutoSizeY();
+	label->setSkin(gSkin);
+	label->setMultiLine(true);
+	listBox->add(label);
+
+	label = createLabel
+	(
+		"Kopplingsavståndet är avståndet från (för lastbilen) "
+		"första axeln och kopplingspunkten. För släpfordon är "
+		"det avståndet från kopplingspunkten och sista axeln.",
+		32
+	);
+	label->setAutoSizeY();
+	label->setSkin(gSkin);
+	label->setMultiLine(true);
+	listBox->add(label);
+
+	label = createLabel
+	(
+		"Vägvänlig fjädring är en axel som har tvillingmontage och "
+		"luftfjädring.",
+		32
+	);
+	label->setAutoSizeY();
+	label->setSkin(gSkin);
+	label->setMultiLine(true);
+	listBox->add(label);
+
+	label = createLabel
+	(
+		"LastBeräkning tar hänsyn till Allmänna begränsningar för "
+		"fordon samt max boggie/trippelaxel avstånd (två respektive "
+		"fem meter).",
+		32
+	);
+	label->setAutoSizeY();
+	label->setSkin(gSkin);
+	label->setMultiLine(true);
 	listBox->add(label);
 
 	// BUG/Counter-intuitive!!
