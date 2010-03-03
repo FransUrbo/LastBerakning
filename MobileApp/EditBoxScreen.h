636df142ca00ea9ad4b070db7c559e3f923547d4
turbo <turbo>
2010-03-03 13:47:07 +0000

    Add some includes and private variables.

--- MobileApp/EditBoxScreen.h
+++ MobileApp/EditBoxScreen.h
@@ -1,26 +1,30 @@
-	/* Copyright (C) 2009 Mobile Sorcery AB
+/* Copyright (C) 2009 Mobile Sorcery AB
 
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License, version 2, as published by
-the Free Software Foundation.
+   This program is free software; you can redistribute it and/or modify it under
+   the terms of the GNU General Public License, version 2, as published by
+   the Free Software Foundation.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+   for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; see the file COPYING.  If not, write to the Free
-Software Foundation, 59 Temple Place - Suite 330, Boston, MA
-02111-1307, USA.
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to the Free
+   Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
 */
 
 #ifndef _EDITBOXSCREEN_H_
 #define _EDITBOXSCREEN_H_
 
 #include <MAUI/Screen.h>
+#include <MAUI/EditBox.h>
 #include <MAUI/ListBox.h>
 #include <MAUI/Layout.h>
+
+#include "CheckBox.h"
+#include "RadioButton.h"
 
 using namespace MAUI;
 
@@ -67,8 +71,17 @@ class EditBoxScreen : public Screen, WidgetListener {
 		void hide();
 
 		Screen *previous;
+
 		ListBox* listBox;
+		ListBox* listBox_select1;
+		ListBox* listBox_select2;
+
 		Layout* mainLayout;
+
+		Vector<EditBox*> editBox;
+		Vector<CheckBox*> checkBox;
+		Vector<RadioButton*> select1;
+		Vector<RadioButton*> select2;
 };
 
 #endif	//_EDITBOXSCREEN_H_
