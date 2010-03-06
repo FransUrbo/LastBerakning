f09bfd168d8a4c8b79cd7b51584f6a1390cd0398
turbo <turbo>
2010-03-06 13:36:40 +0000

    * Only do calculation and create ResultScreen if we've shosen the correct   Label. * Define variables, initialize them (retreive values).   + No point in getTruckWeight() - can just as well do it directly in     doCalculation().

--- MobileApp/screen.cpp
+++ MobileApp/screen.cpp
@@ -1,8 +1,8 @@
 /*
  * screen.cpp
  *
- * $Id: screen.cpp,v 1.8 2010-03-06 00:22:45 turbo Exp $
- * $Revision: 1.8 $
+ * $Id: screen.cpp,v 1.9 2010-03-06 13:36:40 turbo Exp $
+ * $Revision: 1.9 $
  *
  * Copyright Turbo Fredriksson <turbo@bayour.com>
  */
@@ -71,11 +71,13 @@ void MyScreen::keyPressEvent(int keyCode) {
 			case MAK_SOFTLEFT:
 			case MAK_RIGHT:
 			case MAK_FIRE:
-				/* ---------------------------------- */
-				doCalculations();
+				if(listBox->getSelectedIndex() == 3) {
+					/* ---------------------------------- */
+					doCalculations();
 
-				/* ---------------------------------- */
-				screens[3] = new ResultScreen(this);
+					/* ---------------------------------- */
+					screens[3] = new ResultScreen(this);
+				}
 
 				/* ---------------------------------- */
 				lprintfln("Showing screen %d", listBox->getSelectedIndex());
@@ -90,22 +92,50 @@ void MyScreen::keyPressEvent(int keyCode) {
 }
 
 void MyScreen::doCalculations() {
-	/* TODO: Do calculations !! */
+	float truck_weight, truck_load_front, truck_axles_front, truck_load_back, truck_axles_back, truck_road_nice, truck_axle, truck_link;
+	float trailer_weight, trailer_load_front, trailer_axles_front, trailer_load_back, trailer_axles_back, trailer_road_nice, trailer_axle, trailer_link;
+
 	lprintfln("------------------");
 	lprintfln("Doing calculations...");
 
 	/* ---------------------------------- */
+	// Get values we need - truck
+	truck_weight      = (float)atof((const char *)editBoxScreens[0]->editBox[0]->getText().c_str());
+	truck_load_front  = (float)atof((const char *)editBoxScreens[0]->editBox[1]->getText().c_str());
+	truck_axles_front = (float)editBoxScreens[0]->group1->getSelectedButton();
+	truck_load_back   = (float)atof((const char *)editBoxScreens[0]->editBox[2]->getText().c_str());
+	truck_axles_back  = (float)editBoxScreens[0]->group2->getSelectedButton();
+	if(editBoxScreens[0]->checkBox[0]->isChecked())
+		truck_road_nice   = (float)1;
+	else
+		truck_road_nice   = (float)0;
+	truck_axle        = (float)atof((const char *)editBoxScreens[0]->editBox[3]->getText().c_str());
+	truck_link        = (float)atof((const char *)editBoxScreens[0]->editBox[4]->getText().c_str());
+
+	/* ---------------------------------- */
+	// Get values we need - trailer
+	trailer_weight      = (float)atof((const char *)editBoxScreens[1]->editBox[0]->getText().c_str());
+	trailer_load_front  = (float)atof((const char *)editBoxScreens[1]->editBox[1]->getText().c_str());
+	trailer_axles_front = (float)editBoxScreens[1]->group1->getSelectedButton();
+	trailer_load_back   = (float)atof((const char *)editBoxScreens[1]->editBox[2]->getText().c_str());
+	trailer_axles_back  = (float)editBoxScreens[1]->group2->getSelectedButton();
+	if(editBoxScreens[1]->checkBox[0]->isChecked())
+		trailer_road_nice   = (float)1;
+	else
+		trailer_road_nice   = (float)0;
+	trailer_axle        = (float)atof((const char *)editBoxScreens[1]->editBox[3]->getText().c_str());
+	trailer_link        = (float)atof((const char *)editBoxScreens[1]->editBox[4]->getText().c_str());
+
+	/* ---------------------------------- */
+	// Default values for debugging purposes
 	weight_bk1 = 18.0; weight_bk2 = 16.7; weight_bk3 = 14.7;
 	load_bk1   =  8.3; load_bk2   =  7.0; load_bk3 =  5.0;
 
 	/* ---------------------------------- */
-	weight_bk1 = getTruckWeight();
+	weight_bk1 = truck_axles_back;
+
+	/* TODO: Do calculations !! */
 
 	lprintfln("Tjänstevikt, bil: %f", weight_bk1);
 	lprintfln("------------------");
-}
-
-float MyScreen::getTruckWeight() {
-	float value = (float)atof((const char *)editBoxScreens[0]->editBox[0]->getText().c_str());
-	return(value);
 }
