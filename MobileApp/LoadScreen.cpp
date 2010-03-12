a56ee55a04a0460ae3bd8080bba5561d33bddf8b
turbo <turbo>
2010-03-12 11:06:01 +0000

    Insert the version at the very end/bottom of the screen. Version taken from a define in Version.h.

--- MobileApp/LoadScreen.cpp
+++ MobileApp/LoadScreen.cpp
@@ -1,7 +1,7 @@
 /*
  * LoadingScreen.cpp
  *
- * $Id: LoadScreen.cpp,v 1.2 2010-03-12 10:49:56 turbo Exp $
+ * $Id: LoadScreen.cpp,v 1.3 2010-03-12 11:06:01 turbo Exp $
  */
 
 #include <MAUtil/Moblet.h>
@@ -12,14 +12,28 @@
 #include "MAUIex.h"
 #include "LoadScreen.h"
 #include "Util.h"
+#include "Version.h"
 
 LoadScreen::LoadScreen(void) {
+	/* ---------------------------------- */
 	maSetDrawTarget(HANDLE_SCREEN);
 
+	MAExtent screenSize = maGetScrSize();
+	int scrHeight = EXTENT_Y(screenSize);
+
+	/* ---------------------------------- */
 	maSetColor(0xffffff);
 	maDrawText(PADDING*2, 40, "Var god vänta några sekunder...");
 	maDrawText(PADDING*2, 60, "Laddar bruttoviktstabellerna.");
 
+	/* ---------------------------------- */
+	String str("Version: ");
+	str.append(VERSION, strlen(VERSION));
+
+	maSetColor(0xff7700);
+	maDrawText(PADDING*2, scrHeight-16, (const char *)str.c_str());
+
+	/* ---------------------------------- */
 	maUpdateScreen();
 }
 
