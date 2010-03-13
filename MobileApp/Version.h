ddb997dbc39c869ffcaa60c507e87382b2253c77
turbo <turbo>
2010-03-13 23:33:24 +0000

    Bump version - build fixes only.

--- MobileApp/Version.h
+++ MobileApp/Version.h
@@ -1,12 +1,12 @@
 /*
  * Version.h
  *
- * $Id: Version.h,v 1.4 2010-03-12 14:23:26 turbo Exp $
+ * $Id: Version.h,v 1.5 2010-03-13 23:33:24 turbo Exp $
  */
 
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSION "0.93"
+#define VERSION "0.93b"
 
 #endif /* VERSION_H_ */
