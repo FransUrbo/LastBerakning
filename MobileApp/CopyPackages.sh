9bc817dc891b2c29c039f0fb6a08f069af694dce
turbo <turbo>
2010-04-12 20:30:41 +0000

    Extract all 'package' directories and create a tarball with all applications.

---
+++ MobileApp/CopyPackages.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DST=`mktemp -d`
+
+cd FinalOutput/Release || exit 1
+find -type d -name package | while read pkg; do find "$pkg" | cpio -vpd $DST > /dev/null 2>&1; done
+
+cd $DST
+find -type d -name package | while read dir; do cd "$DST/$dir/.."; mv package/* .; rmdir "package"; done
+
+cd $DST
+find -type f -name config.h | while read cfg; do rm "$cfg"; done
+find -type f -print0 | sort | xargs -0 tar cjf ../LastBerakning.tar.bz2
+
+echo "DST: '$DST'"
