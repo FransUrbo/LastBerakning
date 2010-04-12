39b226b894caaf4a47554811a7e364b3e3463d79
turbo <turbo>
2010-04-12 21:15:37 +0000

    Create a tarball with the version in it's name.

--- MobileApp/CopyPackages.sh
+++ MobileApp/CopyPackages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 DST=`mktemp -d`
+VER=`grep 'define VERSION "' Version.h | sed -e 's@.* "@@' -e 's@"@@'`
 
 cd FinalOutput/Release || exit 1
 find -type d -name package | while read pkg; do find "$pkg" | cpio -vpd $DST > /dev/null 2>&1; done
@@ -10,6 +11,6 @@ find -type d -name package | while read dir; do cd "$DST/$dir/.."; mv package/* 
 
 cd $DST
 find -type f -name config.h | while read cfg; do rm "$cfg"; done
-find -type f -print0 | sort | xargs -0 tar cjf ../LastBerakning.tar.bz2
+find -type f -print0 | sort | xargs -0 tar cjf ../LastBerakning-$VER.tar.bz2
 
-echo "DST: '$DST'"
+echo "File: '/tmp/LastBerakning-$VER.tar.bz2'"
