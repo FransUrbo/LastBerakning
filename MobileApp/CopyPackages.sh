30819499aaa53b6553fd8385df50ee96d7b5562a
turbo <turbo>
2010-04-13 14:14:05 +0000

    * Output progress info * Create the tarball with root dir 'LastBerakning-VER'

--- MobileApp/CopyPackages.sh
+++ MobileApp/CopyPackages.sh
@@ -3,14 +3,25 @@
 DST=`mktemp -d`
 VER=`grep 'define VERSION "' Version.h | sed -e 's@.* "@@' -e 's@"@@'`
 
+mkdir "$DST/LastBerakning-$VER"
+DST="$DST/LastBerakning-$VER"
+
+echo -n "Copying packages: "
 cd FinalOutput/Release || exit 1
 find -type d -name package | while read pkg; do find "$pkg" | cpio -vpd $DST > /dev/null 2>&1; done
+echo "done."
 
+echo -n "Cleaning up: "
 cd $DST
 find -type d -name package | while read dir; do cd "$DST/$dir/.."; mv package/* .; rmdir "package"; done
 
 cd $DST
-find -type f -name config.h | while read cfg; do rm "$cfg"; done
-find -type f -print0 | sort | xargs -0 tar cjf ../LastBerakning-$VER.tar.bz2
+find -type f -name config.h -o -name '*.jad' | while read cfg; do rm "$cfg"; done
+echo "done."
 
-echo "File: '/tmp/LastBerakning-$VER.tar.bz2'"
+echo -n "Creating tarball: "
+cd $DST/..
+find -type f -print0 | sort | xargs -0 tar cjf /tmp/LastBerakning-$VER.tar.bz2
+echo "done."
+
+echo ; echo "File: '/tmp/LastBerakning-$VER.tar.bz2'"
