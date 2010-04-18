bd7811c9dfd3615297af70df391cb4ab5a13ae24
turbo <turbo>
2010-04-18 10:32:40 +0000

    * Put the child index of ListBox in a define to simplify changes (if/when I get   touch in createSoftKeyBar() to work). * Fix height of labels and *Box'es to fit new, bigger font * Axle type radio group is now a TouchListBox  to be able to click/touch it. * Implement pointerPressEvent() to be able to catch clicks/touches.

--- MobileApp/EditBoxScreen.cpp
+++ MobileApp/EditBoxScreen.cpp
@@ -18,7 +18,7 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 /* Modified by Turbo Fredriksson <turbo@bayour.com>
  *
  * This screen is the main input data screen.
- * $Id: EditBoxScreen.cpp,v 1.9 2010-03-17 10:12:28 turbo Exp $
+ * $Id: EditBoxScreen.cpp,v 1.10 2010-04-18 10:32:40 turbo Exp $
  */
 
 #include <conprint.h> /* lprintfln() */
@@ -36,35 +36,35 @@ EditBoxScreen::EditBoxScreen(Screen *previous) : previous(previous) {
 
 	/* Create the main work/text area */
 	mainLayout = createMainLayout("Radera", "Tillbaka");
-	listBox = (ListBox*) mainLayout->getChildren()[0];
+	listBox = (ListBox*) mainLayout->getChildren()[FIRSTCHILD];
 
 	/* Create the different label/input fields */
 
 	/* ---------------------------------- */
-	label = createLabel("Tjänstevikt", 64);
-	editBox.add(new EditBox(0, 24, label->getWidth()-PADDING*2, 64-24-PADDING*2,
+	label = createLabel("Tjänstevikt", (FONTHEIGHT*2)-(PADDING*2));
+	editBox.add(new EditBox(0, FONTHEIGHT-(PADDING*2), label->getWidth()-PADDING*2, (FONTHEIGHT*2)-FONTHEIGHT-(PADDING*2),
 							label, "", 0, gFont, true, false, 8, EditBox::IM_NUMBERS));
 	editBox[0]->setDrawBackground(false);
 	label->addWidgetListener(this);
 	listBox->add(label);
 
 	/* ---------------------------------- */
-	label = createLabel("Max belastning, framaxel", 64);
-	editBox.add(new EditBox(0, 24, label->getWidth()-PADDING*2, 64-24-PADDING*2,
+	label = createLabel("Max belastning, framaxel", (FONTHEIGHT*3)-(PADDING*3));
+	editBox.add(new EditBox(0, (FONTHEIGHT*2)-(PADDING*4), label->getWidth()-PADDING*2, (FONTHEIGHT*2)-FONTHEIGHT-(PADDING*2),
 							label, "", 0, gFont, true, false, 8, EditBox::IM_NUMBERS));
 	editBox[1]->setDrawBackground(false);
 	label->addWidgetListener(this);
 	listBox->add(label);
 
 	/* ---------------------------------- */
-	label = createLabel("Axeltyp, framaxel", (32+(3*(20+PADDING))));
-	listBox_select1 = new ListBox(	0, 32, label->getWidth()-PADDING*2, label->getHeight(),
-									label, ListBox::LBO_VERTICAL, ListBox::LBA_NONE, false);
+	label = createLabel("Axeltyp, framaxel", (FONTHEIGHT+(3*(RADIOHEIGHT+PADDING))));
+	listBox_select1 = new TouchListBox(	0, FONTHEIGHT, label->getWidth()-PADDING*2, label->getHeight(), label);
+	listBox_select1->setEnabled(true);
 	group1 = new RadioButtonGroup();
 
 	for(int i = 0; i < 3; i++) {
-		select1.add(new RadioButton(PADDING, (PADDING / 2) + ((i+1)*20), label->getWidth()-PADDING*2,
-									20, listBox_select1, RES_RADIOBUTTON_SELECTED, RES_RADIOBUTTON_UNSELECTED));
+		select1.add(new RadioButton(PADDING, (PADDING / 2) + ((i+1)*RADIOHEIGHT), label->getWidth()-PADDING*2,
+									RADIOHEIGHT, listBox_select1, RES_RADIOBUTTON_SELECTED, RES_RADIOBUTTON_UNSELECTED));
 		group1->addRadioButton(select1[i]);
 	}
 
@@ -76,22 +76,22 @@ EditBoxScreen::EditBoxScreen(Screen *previous) : previous(previous) {
 	listBox->add(label);
 
 	/* ---------------------------------- */
-	label = createLabel("Max belastning, bakaxel", 64);
-	editBox.add(new EditBox(0, 24, label->getWidth()-PADDING*2, 64-24-PADDING*2,
+	label = createLabel("Max belastning, bakaxel", (FONTHEIGHT*3)-(PADDING*3));
+	editBox.add(new EditBox(0, (FONTHEIGHT*2)-(PADDING*4), label->getWidth()-PADDING*2, (FONTHEIGHT*2)-FONTHEIGHT-(PADDING*2),
 							label, "", 0, gFont, true, false, 8, EditBox::IM_NUMBERS));
 	editBox[2]->setDrawBackground(false);
 	label->addWidgetListener(this);
 	listBox->add(label);
 
 	/* ---------------------------------- */
-	label = createLabel("Axeltyp, bakaxel", (32+(3*(20+PADDING))));
-	listBox_select2 = new ListBox(	0, 32, label->getWidth()-PADDING*2, label->getHeight(),
-									label, ListBox::LBO_VERTICAL, ListBox::LBA_NONE, false);
+	label = createLabel("Axeltyp, bakaxel", (FONTHEIGHT+(3*(RADIOHEIGHT+PADDING))));
+	listBox_select2 = new TouchListBox(	0, FONTHEIGHT, label->getWidth()-PADDING*2, label->getHeight(), label);
+	listBox_select2->setEnabled(true);
 	group2 = new RadioButtonGroup();
 
 	for(int i = 0; i < 3; i++) {
-		select2.add(new RadioButton(PADDING, (PADDING / 2) + ((i+1)*20), label->getWidth()-PADDING*2,
-									20, listBox_select2, RES_RADIOBUTTON_SELECTED, RES_RADIOBUTTON_UNSELECTED));
+		select2.add(new RadioButton(PADDING, (PADDING / 2) + ((i+1)*RADIOHEIGHT), label->getWidth()-PADDING*2,
+									RADIOHEIGHT, listBox_select2, RES_RADIOBUTTON_SELECTED, RES_RADIOBUTTON_UNSELECTED));
 		group2->addRadioButton(select2[i]);
 	}
 
@@ -103,22 +103,22 @@ EditBoxScreen::EditBoxScreen(Screen *previous) : previous(previous) {
 	listBox->add(label);
 
 	/* ---------------------------------- */
-	label = createLabel("Vägvänlig fjädring", 32);
+	label = createLabel("Vägvänlig fjädring", FONTHEIGHT);
 	checkBox.add(new CheckBox(scrWidth - 50, 2, 16, 16, label));
 	checkBox[0]->setResources(RES_CHECKBOX_UNCHECKED, RES_CHECKBOX_CHECKED);
 	listBox->add(label);
 
 	/* ---------------------------------- */
-	label = createLabel("Axelavstånd", 64);
-	editBox.add(new EditBox(0, 24, label->getWidth()-PADDING*2, 64-24-PADDING*2,
-							label, "", 0, gFont, true, false, 64, EditBox::IM_NUMBERS));
+	label = createLabel("Axelavstånd", (FONTHEIGHT*2)-(PADDING*2));
+	editBox.add(new EditBox(0, FONTHEIGHT-(PADDING*2), label->getWidth()-PADDING*2, (FONTHEIGHT*2)-FONTHEIGHT-(PADDING*2),
+							label, "", 0, gFont, true, false, (FONTHEIGHT*2), EditBox::IM_NUMBERS));
 	editBox[3]->setDrawBackground(false);
 	label->addWidgetListener(this);
 	listBox->add(label);
 
 	/* ---------------------------------- */
-	label = createLabel("Kopplingsavstånd", 64);
-	editBox.add(new EditBox(0, 24, label->getWidth()-PADDING*2, 64-24-PADDING*2,
+	label = createLabel("Kopplingsavstånd", (FONTHEIGHT*2)-(PADDING*2));
+	editBox.add(new EditBox(0, FONTHEIGHT-(PADDING*2), label->getWidth()-PADDING*2, (FONTHEIGHT*2)-FONTHEIGHT-(PADDING*2),
 							label, "", 0, gFont, true, false, 8, EditBox::IM_NUMBERS));
 	editBox[4]->setDrawBackground(false);
 	label->addWidgetListener(this);
@@ -226,4 +226,28 @@ void EditBoxScreen::keyPressEvent(int keyCode, int nativeCode) {
 #ifdef DEBUG1
 	lprintfln("keyPressEvent() done...");
 #endif
+}
+
+void EditBoxScreen::pointerPressEvent(MAPoint2d point) {
+	Point p;
+	p.set(point.x, point.y);
+
+	for(int i = 0; i < listBox->getChildren().size(); i++) {
+		if(listBox->getChildren()[i]->contains(p)) {
+			listBox->setSelectedIndex(i);
+
+			if (listBox->getSelectedIndex() == 5) {
+#ifdef DEBUG1
+				lprintfln("Flipping checkbox (touch)...");
+#endif
+				CheckBox * cb = (CheckBox *)((Label*)listBox->getChildren()[listBox->getSelectedIndex()])->getChildren()[0];
+				cb->flip();
+#ifdef DEBUG1
+				lprintfln("flipped (touch)...");
+#endif
+			}
+
+			break;
+		}
+	}
 }
