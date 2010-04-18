b793511c9228c9718d2c047acebff03cf4206ef7
turbo <turbo>
2010-04-18 10:41:56 +0000

    Bump version +2 to indicate touch support and bigger font.

--- MobileApp/Version.h
+++ MobileApp/Version.h
@@ -1,12 +1,12 @@
 /*
  * Version.h
  *
- * $Id: Version.h,v 1.10 2010-04-12 21:11:27 turbo Exp $
+ * $Id: Version.h,v 1.11 2010-04-18 10:41:56 turbo Exp $
  */
 
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSION "0.95.3"
+#define VERSION "0.95.5"
 
 #endif /* VERSION_H_ */
